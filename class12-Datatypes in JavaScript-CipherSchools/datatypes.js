9b832b1696ddefad44e432c5eb133020f9673b87
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-02-15 23:17:48 +0530

    Update datatypes.js

--- class12-Datatypes in JavaScript-CipherSchools/datatypes.js
+++ class12-Datatypes in JavaScript-CipherSchools/datatypes.js
@@ -1,15 +1,15 @@
 // String
 let string = "Text";
-console.log("string:",string);
+console.log(" string:",string);
 // console.log("string:", typeof string); typeof is used to find the type of variable
 
 // integer
 let integer = 13;
-console.log("number/ineger:",integer);
+console.log(" number/ineger:",integer);
 
 // float value
 let float = 3.8;
-console.log("float value:",float);
+console.log(" float value:",float);
 
 // boolean value
 let boolean = true;
